7b01fdaab7b47d34b3fd92b2d75faa990d4c41a7
mst-mkt <s1f102303057@iniad.org>
2023-11-27 16:43:30 +0900

    add: url params

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, "blog/index.html")
+
+
+def update(request: HttpRequest, article_id: int) -> HttpResponse:
+    return HttpResponse(f"article_id: {article_id}")
