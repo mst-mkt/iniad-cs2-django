0fe8bb42e29b41b96816ac46a85f66a4003c8e53
mst-mkt <s1f102303057@iniad.org>
2023-11-28 21:26:42 +0900

    add: Article model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    posted_at = models.DateTimeField(default=timezone.now)
+    published_at = models.DateTimeField(null=True, blank=True)
+    like = models.IntegerField(default=0)
+
+    def __str__(self: "Article") -> str:
+        return self.title
+
+    def publish(self: "Article") -> None:
+        self.published_at = timezone.now()
+        self.save()
